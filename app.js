7999b2c10365d60378746fffbf254075a548d8ba
Gulhasanli1993 <gulshanhasanli641@gmail.com>
2025-08-15 06:12:35 +0000

    js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 $(document).ready(function () {
     $('.btn').click(function (e) {
         e.preventDefault();
-        if (!$('input').trim(val()) = '')  {
-        let a = b => {
-            for (let i = 1; i<=b; i++) { $('.minibox').append(`<h2>${b}</h2>`); } 
+        if (!$('input').trim(val()) == '') {
+            let a = b => {
+                for (let i = 0; i < b; i++) { $('.minibox').append(`<h2>${b}</h2>`); }
             }
-let v = f => ••• ? ••• : $('.minibox').append(`<h2>${f}</h2>`);
-v($('input').val()); 
+            let v = f => f() ? a : $('.minibox').append(`<h2>${f}</h2>`);
+            v($('input').val());
         }
-        $('input').val('')
-    }) 
+        $('input').val()
+    })
 });
